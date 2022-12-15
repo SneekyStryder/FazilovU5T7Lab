55644b65add2735a91c30dbd523a048123dc7b32
Imran Fazilov <ifazilov3584@bths.edu>
2022-12-15 12:22:23 -0500

    lab done

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String firstName; private String lastName;
+    private static int recentIdNum = 100; private static int numHired = 0;
+    private int idNum;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        recentIdNum++;
+        idNum = recentIdNum;
+        numHired++;
+    }
+
+    public static int getRecentIdNum() {
+        return recentIdNum;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getIdNum() {
+        return idNum;
+    }
+
+    public static int getNumHired() {
+        return numHired;
+    }
+
+    public String employeeInfo() {
+        return "Employee's full name: " + fullName() + "\nEmployee's ID: " + idNum;
+    }
+
+    public static String classInfo() {
+        return "Most recent ID assigned: " + recentIdNum + "\nTotal employees hired: " + numHired;
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private static int totalPointsEarned = 0; private static int greatestPoints = 0;
+    private String name;
+    private int numPoints = 0;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void addPoints(int points) {
+        numPoints += points;
+        totalPointsEarned += points;
+        if (numPoints > greatestPoints) {
+            greatestPoints = numPoints;
+        }
+    }
+
+    public String studentInfo() {
+        return "Student: " + name + "\nPoints: " + numPoints;
+    }
+
+    public static int getTotalPointsEarned() {
+        return totalPointsEarned;
+    }
+
+    public static int getGreatestPoints() {
+        return greatestPoints;
+    }
 }
